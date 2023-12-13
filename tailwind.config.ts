8a861ad9e7e5f7dd9e5905a26d054a1285a13990
Mikael <bernardesmikael@outlook.com>
2023-12-12 22:27:43 -0300

    config: colors in tailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,17 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
 	content: [
-		'./src/app/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
-		extend: {
-		},
+		colors: {
+			B100: '#1E1E1E',
+			B300: '#868D98',
+			T100: '#BDC3CB',
+			T300: '#E1E8F0',
+			P100: '#7288FF'
+		}
 	},
 	plugins: [],
 };
