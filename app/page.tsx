f13ec2a4cfd629454d15126d9a2845265bcf7b2a
Mikael <bernardesmikael@outlook.com>
2023-12-12 23:20:09 -0300

    feat: add text for apresentation in first screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+
 export default function Home() {
 	return (
-		<main>
-			
+		<main className='w-full h-fit text-xs font-light text-T100 flex flex-wrap px-8 mt-6 md:text-lg'>
+			<p>Hi, I'm <span className='text-P100 font-semibold'>Mikael Bernardes</span>, a <span className='text-P100 font-semibold'>frontend developer</span> specializing in <span className='text-P100 font-semibold'>ReactJS</span>, with more than <span className='text-P100 font-semibold'>2 years of experience</span>in the web development area. My portfolio is not only a showcase of my work, but also a space to share insights through my blog. My goal is to become a senior frontend developer, contributing to innovative projects globally. In addition to code, I dedicate time to classical guitar, believing that creativity transcends programming and music.</p>
 		</main>
 	);
 }
