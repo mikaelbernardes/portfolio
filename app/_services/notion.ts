a393e9ab6235a7ea2f929fc85fbb8d3d55fdfd9e
Mikael <bernardesmikael@outlook.com>
2023-12-17 20:43:36 -0300

    feat: add the logic for posts page

--- app/_services/notion.ts
+++ app/_services/notion.ts
@@ -1,13 +1,14 @@
 // 'use server';
 import { Client } from '@notionhq/client';
 import { NotionDatabaseResponse } from './types';
+import { NotionToMarkdown } from 'notion-to-md';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const DATABASE_ID = '75fd422eb8bf44a888aa7f157b373062';
 
 export async function getPosts() {
-	const databaseId = '75fd422eb8bf44a888aa7f157b373062';
 	const response = await notion.databases.query({
-		database_id: databaseId,
+		database_id: DATABASE_ID,
 	});
 
 	const typedResponse = (response as unknown) as NotionDatabaseResponse;
@@ -16,9 +17,9 @@ export async function getPosts() {
 		const title = post.properties.title?.title?.[0]?.text.content || 'Untitled';
 		const slug = post.properties.slug?.rich_text?.[0]?.plain_text || 'no-slug';
 		const tags =
-		post.properties.tags && post.properties.tags.multi_select
-		  ? post.properties.tags.multi_select.map((tag) => tag.name)
-		  : [''];
+			post.properties.tags && post.properties.tags.multi_select
+				? post.properties.tags.multi_select.map((tag) => tag.name)
+				: [''];
 
 		return {
 			id: post.id,
@@ -28,4 +29,37 @@ export async function getPosts() {
 			createdAt: post.created_time,
 		};
 	});
+}
+
+export async function getPost(slug: string) {
+	const response = await notion.databases.query({
+		database_id: DATABASE_ID,
+		filter: {
+			or: [
+				{
+					property: 'slug',
+					rich_text: {
+						equals: slug,
+					},
+				},
+			],
+		},
+	});
+
+	const pageId = response.results[0].id;
+
+	const n2m = new NotionToMarkdown({ notionClient: notion });
+
+	const mdblocks = await n2m.pageToMarkdown(pageId);
+	const mdString = n2m.toMarkdownString(mdblocks);
+
+	const typedResponse = response as unknown as NotionDatabaseResponse;
+
+	console.log(pageId);
+	
+
+	return {
+		title: typedResponse.results[0].properties.title.title[0].plain_text,
+		content: mdString.parent,
+	};
 }
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,27 @@
-export default function Post () {
+'use client';
+import ReactMarkdown from 'react-markdown';
+import { getPost } from '../../_services/notion';
+
+export default async function Post ({
+	params,
+}: {
+	params: {
+		slug:string
+	}
+}){
+
+	const post = await getPost(params.slug);
+
 	return(
-		<>
-        huuuuhuuhuhuhuh
-		</>
+		<main className='w-screen h-screen flex flex-col'>
+			<h1 className='text-P100 text-5xl mt-6 mb-6'>{post.title}</h1>
+			<section className='text-T300'>
+				<ReactMarkdown
+					components={{
+						h2: ({ ...props }) => <h2 className='text-2xl' {...props}/>
+					}}
+				>{post.content}</ReactMarkdown>
+			</section>
+		</main>
 	);
 }
