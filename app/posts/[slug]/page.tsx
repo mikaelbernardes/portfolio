f264a323cac78df741ff643945ab5a1a1f61ca48
Mikael <bernardesmikael@outlook.com>
2023-12-17 22:25:55 -0300

    fix: the code view in post detail page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,26 +1,67 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
-import ReactMarkdown from 'react-markdown';
 import { getPost } from '../../_services/notion';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import rehypeStringify from 'rehype-stringify';
+import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { CSSProperties } from 'react';
 
-export default async function Post ({
+export default async function Post({
 	params,
 }: {
 	params: {
-		slug:string
-	}
-}){
-
+		slug: string;
+	};
+}) {
 	const post = await getPost(params.slug);
 
-	return(
-		<main className='w-screen h-screen flex flex-col'>
+	const renderers = {
+		strong: ({ node, children }: any) => {
+			return <span className='text-T300 font-bold'>{children}</span>;
+		},
+		code: ({ node, className, children, ...props }: any) => {
+			const match = /language-(\w+)/.exec(className || '');
+
+			if (!match) {
+				return (
+					<code className={className} {...props}>
+						{children}
+					</code>
+				);
+			}
+
+			const style: { [key: string]: CSSProperties } = dracula;
+
+			const syntaxHighlighterProps = {
+				language: match[1],
+				PreTag: 'div',
+				style: style,
+				...props,
+			};
+
+			return (
+				<SyntaxHighlighter {...syntaxHighlighterProps as any}>
+					{String(children).replace(/\n$/, '')}
+				</SyntaxHighlighter>
+			);
+		},
+	};
+
+	return (
+		<main className='w-full min-h-screen flex flex-col pb-6 px-6 lg:px-0'>
 			<h1 className='text-P100 text-5xl mt-6 mb-6'>{post.title}</h1>
-			<section className='text-T300'>
+			<section className='text-T100 flex flex-col gap-4'>
 				<ReactMarkdown
-					components={{
-						h2: ({ ...props }) => <h2 className='text-2xl' {...props}/>
-					}}
-				>{post.content}</ReactMarkdown>
+					remarkPlugins={[gfm]}
+					rehypePlugins={[rehypeRaw, rehypeStringify]}
+					components={renderers}
+				>
+					{post.content}
+				</ReactMarkdown>
 			</section>
 		</main>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,15 @@ const config: Config = {
 		'./src/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
-		colors: {
-			B100: '#1E1E1E',
-			B300: '#868D98',
-			T100: '#BDC3CB',
-			T300: '#E1E8F0',
-			P100: '#7288FF',
-			Back: '#662079'
+		extend: {
+			colors: {
+				B100: '#1E1E1E',
+				B300: '#868D98',
+				T100: '#BDC3CB',
+				T300: '#E1E8F0',
+				P100: '#7288FF',
+				Back: '#662079'
+			}
 		}
 	},
 	plugins: [],
