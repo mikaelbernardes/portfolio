8e37cad9bf2030fcdfd6ed48c588a5ba0deda8df
Mikael <bernardesmikael@outlook.com>
2023-12-22 01:27:26 -0300

    fix: try and catch for the post screen

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,29 +5,30 @@ import { getPosts } from '../_services/notion';
 import { PostProps } from '@/types/posts';
 
 export default async function PostsHome() {
-	try {
-		const allPosts = await getPosts();
+	let allPosts: PostProps[] = [];
 
-		return (
-			<main className='w-full h-fit text-xs font-light text-T100 flex flex-col px-4 md:px-16 mt-6 md:text-lg pb-6 gap-5'>
-				<div className='flex items-center gap-4 md:gap-10'>
-					<p className='text-T100 text-lg md:text-2xl font-bold'>Posts</p>
-				</div>
-				<ul className='flex flex-col gap-6'>
-					{allPosts.map((post: PostProps) => (
-						<LinkPost
-							key={post.id}
-							title={post.title}
-							createdAt={new Intl.DateTimeFormat('en-US').format(new Date(post.createdAt))}
-							slug={post.slug}
-							tags={post.tags}
-						/>
-					))}
-				</ul>
-			</main>
-		);
+	try {
+		allPosts = await getPosts();
 	} catch (error) {
 		console.error('Error fetching posts:', error);
-		return null;
 	}
+
+	return (
+		<main className='w-full h-fit text-xs font-light text-T100 flex flex-col px-4 md:px-16 mt-6 md:text-lg pb-6 gap-5'>
+			<div className='flex items-center gap-4 md:gap-10'>
+				<p className='text-T100 text-lg md:text-2xl font-bold'>Posts</p>
+			</div>
+			<ul className='flex flex-col gap-6'>
+				{allPosts.map((post: PostProps) => (
+					<LinkPost
+						key={post.id}
+						title={post.title}
+						createdAt={new Intl.DateTimeFormat('en-US').format(new Date(post.createdAt))}
+						slug={post.slug}
+						tags={post.tags}
+					/>
+				))}
+			</ul>
+		</main>
+	);
 }
