1c60a7cd7b0cec50b55c20b20d912c381240a924
Mikael <bernardesmikael@outlook.com>
2024-11-17 15:39:42 -0300

    feat: add pagination in post and project page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { PostCard } from "@/components/post-card";
 import { useEffect, useState } from "react";
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
 	const [posts, setPosts] = useState<Post[]>([]);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const postsPerPage = 4;
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -20,16 +32,53 @@ export default function Page() {
 		fetchData();
 	}, []);
 
+	const filteredPosts = posts.filter((post) => post.isBlog);
+	const indexOfLastPost = currentPage * postsPerPage;
+	const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+	const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+
 	return (
 		<div className="container mx-auto px-4 py-8 flex flex-col gap-4 items-center">
-			{posts
-				.filter((post) => post.isBlog)
-				.map((post) => (
-					<PostCard
-						post={post}
-						key={post.id}
-					/>
-				))}
+			{currentPosts.map((post) => (
+				<PostCard
+					post={post}
+					key={post.id}
+				/>
+			))}
+			<Pagination>
+				<PaginationContent>
+					<PaginationItem>
+						<Button
+							variant="ghost"
+							onClick={() => setCurrentPage(currentPage - 1)}
+							disabled={currentPage === 1}>
+							<PaginationPrevious />
+						</Button>
+					</PaginationItem>
+					{Array.from(
+						{ length: Math.ceil(posts.length / postsPerPage) },
+						(_, index) => (
+							<PaginationItem key={index}>
+								<PaginationLink
+									href="#"
+									onClick={() => setCurrentPage(index + 1)}
+									className={currentPage === index + 1 ? "bg-secondary" : ""}>
+									{index + 1}
+								</PaginationLink>
+							</PaginationItem>
+						),
+					)}
+					<PaginationItem>
+						<Button
+							variant="ghost"
+							onClick={() => setCurrentPage(currentPage + 1)}
+							disabled={indexOfLastPost >= posts.length}>
+							<PaginationNext />
+						</Button>
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
 		</div>
 	);
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,21 @@
 "use client";
-import { ProjectCard } from "@/components/project-card";
+import { PostCard } from "@/components/post-card";
 import { useEffect, useState } from "react";
+import {
+	Pagination,
+	PaginationContent,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
+import { ProjectCard } from "@/components/project-card";
 
 export default function Page() {
 	const [posts, setPosts] = useState<Post[]>([]);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const postsPerPage = 4;
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -20,16 +32,56 @@ export default function Page() {
 		fetchData();
 	}, []);
 
+	const filteredPosts = posts.filter(
+		(post) => !post.isBlog && !post.isProfessionalExperience,
+	);
+	const indexOfLastPost = currentPage * postsPerPage;
+	const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+	const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+
 	return (
 		<div className="container mx-auto px-4 py-8 flex flex-col gap-4 items-center">
-			{posts
-				.filter((post) => !post.isBlog)
-				.map((post) => (
-					<ProjectCard
-						project={post}
-						key={post.id}
-					/>
-				))}
+			{currentPosts.map((post) => (
+				<ProjectCard
+					isProfessionalExperience={false}
+					project={post}
+					key={post.id}
+				/>
+			))}
+			<Pagination>
+				<PaginationContent>
+					<PaginationItem>
+						<Button
+							variant="ghost"
+							onClick={() => setCurrentPage(currentPage - 1)}
+							disabled={currentPage === 1}>
+							<PaginationPrevious />
+						</Button>
+					</PaginationItem>
+					{Array.from(
+						{ length: Math.ceil(posts.length / postsPerPage) },
+						(_, index) => (
+							<PaginationItem key={index}>
+								<PaginationLink
+									href="#"
+									onClick={() => setCurrentPage(index + 1)}
+									className={currentPage === index + 1 ? "bg-secondary" : ""}>
+									{index + 1}
+								</PaginationLink>
+							</PaginationItem>
+						),
+					)}
+					<PaginationItem>
+						<Button
+							variant="ghost"
+							onClick={() => setCurrentPage(currentPage + 1)}
+							disabled={indexOfLastPost >= posts.length}>
+							<PaginationNext />
+						</Button>
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
 		</div>
 	);
 }
